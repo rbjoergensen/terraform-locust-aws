b9d6535161a9097a7d4bcdc64165345398b932d6
Rasmus Jørgensen <rbjoergensen@users.noreply.github.com>
2022-04-21 23:48:51 +0200

    Update tasks.py

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -1,23 +1,8 @@
 from locust import HttpUser, TaskSet, task, between
 import random
 import json
-import csv
-
-#tokens = []
-
-#with open('tokens.csv') as csvfile:
-#    readCSV = csv.reader(csvfile, delimiter = ',')
-#    for row in readCSV:
-#        tokens.append({
-#            'token': row[0],
-#        })
-
-#def getActor():
-#    return random.choice(tokens)
-
 
 class MyTask(HttpUser):
-
     host = 'https://callofthevoid.dk'
  
     wait_time = between(3, 9)
@@ -29,19 +14,3 @@ class MyTask(HttpUser):
     @task(3) 
     def image(self):
         self.client.get('/robot_remaster.gif')
-
-    #def on_start(self):
-    #    #self.actor = 'test' #getActor()
-    #    self.index()
-
-    #def getHeaders(self):
-    #    headers = {
-    #        'Authorization': 'Bearer ' + self.actor['token'],
-    #        'content-type': 'application/json'
-    #    }
-    #    return headers
-
-    #@task(3)
-    #def somethingelse(self):
-        #self.client.get('/endpoint/something/else', headers = self.getHeaders())
-        #self.client.get('/endpoint/something/1', headers = self.getHeaders())
